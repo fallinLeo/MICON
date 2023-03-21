999a301eeb9d2b390a40bbe6f9e35b1016d01ddb
fallinLeo <gogorabbit1@gmail.com>
2023-03-21 17:00:48 +0900

    Micon-3th
    3Th_first code

--- MICON_Sketch/MICON_Sketch.ino
+++ MICON_Sketch/MICON_Sketch.ino
@@ -1,16 +1,25 @@
 
+
+
+#define TRIG 9
+#define ECHO 8
+//=======motor pin set
 const int AIA = 11;
 const int AIB = 6;
 const int BIA = 3;
 const int BIB = 5;
-int Speed = 200;
-long first1=millis();
-long last1;
+//=======motor pin set end
+int Speed = 150;
+
+long first1=millis();//timer1
+long last1;//timer1
 
 
+void forward();
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(AIA, OUTPUT);
+  //AIA,AIB ==> right direction, velocity
+  //BIA,BIB ==> left direction,velocity
+  pinMode(AIA, OUTPUT); //motor setting(output)
   pinMode(AIB, OUTPUT);
   pinMode(BIA, OUTPUT);
   pinMode(BIB, OUTPUT);
@@ -18,19 +27,35 @@ void setup() {
 
 void loop() {
   last1 = millis();
-  if(first1<=5000)
+  //left();
+  if(first1-last1<=5000)
   {
     forward();
   }
 }
 
 
+void left()
+{
+  analogWrite(AIA,0);
+  analogWrite(AIB,Speed);
+  analogWrite(BIA,0);
+  analogWrite(BIB,Speed);
+}
+void right()
+{
+  analogWrite(AIA,0);
+  analogWrite(AIB,Speed);
+  analogWrite(BIA,Speed);
+  analogWrite(BIB,0);
+}
 void forward() {
 	analogWrite(AIA, 0);
 	analogWrite(AIB, Speed);
 	analogWrite(BIA, 0);
 	analogWrite(BIB, Speed);
 }
+
 void brake(float t)
 {
   analogWrite(AIA,0);
@@ -38,3 +63,17 @@ void brake(float t)
   analogWrite(BIA,0);
   analogWrite(BIB,0);
 }
+
+
+/*float Super_Sonic()
+{
+  long duration, distance;
+  digitalWrite(TRIG,LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG,LOW);
+  duration = pulseIn(ECHO.HIGH);
+  distance = duration*17/1000;
+  return distance;
+}*/
